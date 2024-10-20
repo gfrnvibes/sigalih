0ea4dc5acc9fc6ae59caa9c41c7aa914b8632818
Gufron <gupron.nurjalil14@gmail.com>
2024-10-20 12:57:03 +0700

    hapus warga seeders

--- database/seeders/WargaSeeder.php
+++ database/seeders/WargaSeeder.php
@@ -17,41 +17,6 @@ class WargaSeeder extends Seeder
      */
     public function run()
     {
-        Warga::create([
-            "nik" => "3205201403010002",
-            "nama" => "Gupron Nurjalil",
-            "ttl" => "Garut, 14/03/2001",
-            "jk" => "Laki - Laki",
-            "alamat" => "Kp. Dungus Maung",
-            "rt" => "02",
-            "rw" => "06",
-            "agama" => "Islam",
-            "status" => "Belum Kawin",
-            "pekerjaan" => "Pelajar/Mahasiswa",
-        ]);
-        Warga::create([
-            "nik" => "3205202711030003",
-            "nama" => "Muhamad Gunawan",
-            "ttl" => "Garut, 27/11/2003",
-            "jk" => "Laki - Laki",
-            "alamat" => "Kp. Dungus Maung",
-            "rt" => "02",
-            "rw" => "06",
-            "agama" => "Islam",
-            "status" => "Belum Kawin",
-            "pekerjaan" => "Pelajar/Mahasiswa",
-        ]);
-        Warga::create([
-            "nik" => "3205203010050004",
-            "nama" => "Arip Ramdani",
-            "ttl" => "Garut, 30/10/05",
-            "jk" => "Laki - Laki",
-            "alamat" => "Kp. Dungus Maung",
-            "rt" => "02",
-            "rw" => "06",
-            "agama" => "Islam",
-            "status" => "Belum Kawin",
-            "pekerjaan" => "Pelajar/Mahasiswa",
-        ]);
+
     }
 }
